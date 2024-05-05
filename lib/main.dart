cd0cf0835909f17ba1c395af5b9e61239fd20f83
Emerson Gonçalves <emerson.u.g@gmail.com>
2024-05-05 17:37:31 -0300

    feat: add system tray

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:window_manager/window_manager.dart';
+import 'package:system_tray/system_tray.dart';
 
 import './theme.dart';
 import './features/settings/pages/settings_pag.dart';
@@ -19,8 +23,75 @@ void main() {
   runApp(const Historian());
 }
 
-class Historian extends StatelessWidget {
+class Historian extends StatefulWidget {
   const Historian({super.key});
+
+  @override
+  State<Historian> createState() => _HistorianState();
+}
+
+class _HistorianState extends State<Historian> {
+  @override
+  void initState() {
+    super.initState();
+
+    initSystemTray();
+  }
+
+  Future<String> get lastCopy async {
+    final clipboardData = await Clipboard.getData(Clipboard.kTextPlain);
+    return clipboardData?.text ?? '';
+  }
+
+  Future<void> initSystemTray() async {
+    String path = Platform.isWindows
+        ? 'assets/icons/app_icon.ico'
+        : 'assets/icons/app_icon.svg';
+
+    final SystemTray systemTray = SystemTray();
+
+    // We first init the systray menu
+    await systemTray.initSystemTray(
+      title: await lastCopy,
+      iconPath: path,
+    );
+
+    // create context menu
+    final Menu menu = Menu();
+    await menu.buildFrom([
+      MenuItemLabel(
+          label: 'Show',
+          onClicked: (menuItem) {
+            WindowManager.instance.show();
+            WindowManager.instance.focus();
+            WindowManager.instance.setAlwaysOnTop(true);
+          }),
+      MenuItemLabel(
+          label: 'Hide',
+          onClicked: (menuItem) => WindowManager.instance.hide()),
+      MenuSeparator(),
+      MenuItemLabel(
+          label: 'Exit',
+          onClicked: (menuItem) => WindowManager.instance.close()),
+    ]);
+
+    // set context menu
+    await systemTray.setContextMenu(menu);
+
+    // handle system tray event
+    systemTray.registerSystemTrayEventHandler((eventName) {
+      debugPrint("eventName: $eventName");
+      if (eventName == kSystemTrayEventClick) {
+        Platform.isWindows
+            ? WindowManager.instance.show()
+            : systemTray.popUpContextMenu();
+      } else if (eventName == kSystemTrayEventRightClick) {
+        Platform.isWindows
+            ? systemTray.popUpContextMenu()
+            : WindowManager.instance.show();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
